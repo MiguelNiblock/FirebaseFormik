ce5649f6507deaf10adcdcaf618bbcace681b1a5
miguelniblock@posteo.net <miguelniblock@posteo.net>
2021-09-22 22:38:11 -0700

    correct disabling of submit and reset buttons

--- App.tsx
+++ App.tsx
@@ -102,7 +102,7 @@ export default ()=> {
           color="black"
           mode="contained"
           loading={isSubmitting}
-          disabled={isSubmitting}
+          disabled={isSubmitting || Object.entries(errors).some(([k,v])=>v) || Object.entries(values).every(([k,v])=>!v)}
           style={{ marginTop: 16 }}>
           Submit
         </Button>
@@ -110,7 +110,7 @@ export default ()=> {
           onPress={handleReset}
           color="black"
           mode="outlined"
-          disabled={isSubmitting}
+          disabled={isSubmitting || Object.entries(values).every(([k,v])=>!v)}
           style={{ marginTop: 16 }}>
           Reset
         </Button></>
